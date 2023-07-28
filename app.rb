83d2f43b66c6c8429c81b8a7079ddf91ce919c45
pgbuker <kirill.bezik@gmail.com>
2023-07-28 15:47:35 +0300

    Rack first attempt

--- app.rb
+++ app.rb
@@ -1,19 +1,20 @@
+require_relative 'timeformat'
+
 class App
+  
   def call(env)
     [status, headers, body]
-  end
-
-  private
-
-  def status
-    300
-  end
-
-  def headers
-    { 'Content-Type' => 'text/plain' }
-  end
-
-  def body
-    ["We need a time!\n"]
+    @request = Rack::Request.new(env)
+    if @request.path_info == '/time'
+      formatter = TimeFormat.new(@request.params)
+      if formatter.incorrect.empty?
+      [200,{"Content-Type" => "text/plain"}, [Time.now.strftime(formatter.correct)]]
+      else
+      [400,{"Content-Type" => "text/plain"}, ["Unknown time format #{formatter.incorrect}, KNOWN PARAMS #{formatter.correct}" ]]
+      end  
+    else
+      [404,{"Content-Type" => "text/plain"}, ["Page not found!\n"]]
+    end
   end
 end
+
